fe7567aff80f067d39a04352ebf6eebaacaddf67
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2023-12-21 12:14:49 +0700

    fixing json array using regex

--- src/test/java/com/so/functionaltest/masterstation/CreateMasterStationTest.java
+++ src/test/java/com/so/functionaltest/masterstation/CreateMasterStationTest.java
@@ -28,8 +28,8 @@ public class CreateMasterStationTest {
 
         JSONObject request = Utility.buildRequestJSON("request/station/create_master_station_success.json");
         String usernameMasterStation = ListMasterStationTest.getUsernameMasterStation();
-        RequestSpecification requestSpecification =  Utility.
-                createRequestWithBodyAndHeaders(request.toJSONString(),ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
+        RequestSpecification requestSpecification = Utility.
+                createRequestWithBodyAndHeaders(request.toJSONString(), ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
                         Collections.singletonMap("TESTING-LOGIN-USER", "STATION_OPERATOR.admin"));
         String tokenWhenUsingLogin = LoginAPI.getTokenWhenUsingLogin();
         requestSpecification.header("Authorization", "Bearer " + tokenWhenUsingLogin);
@@ -47,12 +47,13 @@ public class CreateMasterStationTest {
         System.out.println(extract.response().getBody().prettyPrint());
 
     }
+
     @Test(testName = "Station_N_011_CreateMasterStation_TokenInvalid")
     public void createMasterStationTokenIsBlank() throws IOException, ParseException {
         JSONObject request = Utility.buildRequestJSON("request/station/create_master_station_success.json");
         String usernameMasterStation = ListMasterStationTest.getUsernameMasterStation();
-        RequestSpecification requestSpecification =  Utility.
-                createRequestWithBodyAndHeaders(request.toJSONString(),ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
+        RequestSpecification requestSpecification = Utility.
+                createRequestWithBodyAndHeaders(request.toJSONString(), ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
                         Collections.singletonMap("TESTING-LOGIN-USER", "STATION_OPERATOR.admin"));
         ExtractableResponse<Response> extract = given(requestSpecification)
                 .log().all().post("/" + usernameMasterStation)
@@ -68,11 +69,11 @@ public class CreateMasterStationTest {
     }
 
     @Test(testName = "Station_N_012_CreateMasterStation_AuthInvalid")
-    public void createMasterStationAuthInvalid() throws  IOException, ParseException {
+    public void createMasterStationAuthInvalid() throws IOException, ParseException {
         JSONObject request = Utility.buildRequestJSON("request/station/create_master_station_success.json");
         String usernameMasterStation = ListMasterStationTest.getUsernameMasterStation();
-        RequestSpecification requestSpecification =  Utility.
-                createRequestWithBodyAndHeaders(request.toJSONString(),ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
+        RequestSpecification requestSpecification = Utility.
+                createRequestWithBodyAndHeaders(request.toJSONString(), ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
                         Collections.singletonMap("TESTING-LOGIN-USER", "ADMIN.admin"));
         String tokenWhenUsingLogin = LoginAPI.getTokenWhenUsingLogin();
         requestSpecification.header("Authorization", "Bearer " + tokenWhenUsingLogin);
@@ -95,8 +96,8 @@ public class CreateMasterStationTest {
         JSONObject request = Utility.buildRequestJSON("request/station/create_master_station_success.json");
         request.remove("stations");
         String usernameMasterStation = ListMasterStationTest.getUsernameMasterStation();
-        RequestSpecification requestSpecification =  Utility.
-                createRequestWithBodyAndHeaders(request.toJSONString(),ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
+        RequestSpecification requestSpecification = Utility.
+                createRequestWithBodyAndHeaders(request.toJSONString(), ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
                         Collections.singletonMap("TESTING-LOGIN-USER", "STATION_OPERATOR.admin"));
         String tokenWhenUsingLogin = LoginAPI.getTokenWhenUsingLogin();
         requestSpecification.header("Authorization", "Bearer " + tokenWhenUsingLogin);
@@ -118,23 +119,26 @@ public class CreateMasterStationTest {
         JSONObject request = Utility.buildRequestJSON("request/station/create_master_station_success.json");
 
         JSONObject joUUid = new JSONObject();
-        joUUid.put("uuid", UUID.randomUUID());
+        joUUid.put("uuid", UUID.randomUUID().toString());
 
         JSONObject jo = new JSONObject();
         jo.put("station", joUUid);
 
         JSONArray ja = new JSONArray();
         ja.put(jo);
+
 //        JSONObject mainObject = new JSONObject();
 //        mainObject.put("stations", ja);
 //        System.out.println(mainObject);
-        System.out.println(request);
         request.put("stations", ja);
-        System.out.println(request);
-
+        String jsonBody = request.toJSONString()
+                .replaceAll("\\[\"","[")
+                .replaceAll("\"]","]")
+                .replaceAll("\\\\", "");
+        System.out.println(jsonBody);
         String usernameMasterStation = ListMasterStationTest.getUsernameMasterStation();
-        RequestSpecification requestSpecification =  Utility.
-                createRequestWithBodyAndHeaders(request.toJSONString(),ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
+        RequestSpecification requestSpecification = Utility.
+                createRequestWithBodyAndHeaders(jsonBody, ConstantParameter.PATH_PARAM_MODULE_MASTER_STATION,
                         Collections.singletonMap("TESTING-LOGIN-USER", "STATION_OPERATOR.admin"));
         String tokenWhenUsingLogin = LoginAPI.getTokenWhenUsingLogin();
         requestSpecification.header("Authorization", "Bearer " + tokenWhenUsingLogin);
